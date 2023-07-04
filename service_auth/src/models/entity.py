f0777b10bd1fb56a855513aa4a020e38ac806d6e
Maks Iamchuk <maks.iamchuk@outlook.com>
2023-07-04 19:12:25 +0400

    SQLAlchemy models;Pydantic validatioin;PG Healtcheck

--- service_auth/src/models/entity.py
+++ service_auth/src/models/entity.py
@@ -1,30 +1,80 @@
+import token
 import uuid
 from datetime import datetime
 
-from sqlalchemy import Boolean, Column, DateTime, String
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, String
 from sqlalchemy.dialects.postgresql import UUID
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from src.db.postgres import Base
 
-class User(Base):
+
+class BaseMixin:
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False
+    )
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class User(Base, BaseMixin):
     __tablename__ = 'users'
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    login = Column(String(255), unique=True, nullable=False)
-    password = Column(String(255), nullable=False)
-    first_name = Column(String(50))
-    last_name = Column(String(50))
-    created_at = Column(DateTime, default=datetime.utcnow)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
+    hash_password = Column(String(255), nullable=False)
+    verified = Column(Boolean, nullable=False, server_default='False')
+    role_id = Column(UUID(as_uuid=True), ForeignKey('roles.id'))
 
-    def __init__(self, login: str, password: str, first_name:str, last_name: str) -> None:
-        self.login = login
-        self.password = self.password = generate_password_hash(password)
+    def __init__(
+            self,
+            first_name: str,
+            last_name: str,
+            email: str,
+            password: str
+    ) -> None:
+
         self.first_name = first_name
         self.last_name = last_name
-    
+        self.email = email
+        self.hash_password = generate_password_hash(password)
+
     def check_password(self, password: str) -> bool:
-        return check_password_hash(self.password, password)
+        return check_password_hash(self.hash_password, password)
 
     def __repr__(self) -> str:
-        return f'<User {self.login}>' 
+        return f'<User {self.email}>'
+
+
+class Role(Base, BaseMixin):
+    __tablename__ = 'roles'
+
+    name = Column(String(15), nullable=False)
+    description = Column(String(255), nullable=False)
+
+    def __init__(self, name: str, description: str) -> None:
+        self.name = name
+        self.description = description
+
+    def __repr__(self) -> str:
+        return f'<User {self.name}>'
+
+
+class AccountHistory(Base, BaseMixin):
+    __tablename__ = 'account_history'
+
+    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'))
+    user_agent = Column(String(50), nullable=False, server_default='default UA')  # здесь потом должна быть функция получающая useragent
+
+
+class RefreshToken(Base, BaseMixin):
+    __tablename__ = 'refresh_tokens'
+
+    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id'))
+    user_token = Column(String(255), nullable=False, server_default='default UT')  # здесь потом должна быть функция получающая user_token
+    is_active = Column(Boolean, nullable=False, server_default='False')
--- service_auth/src/schemas/entity.py
+++ service_auth/src/schemas/entity.py
@@ -1,13 +1,35 @@
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, validator
 
 
 class UserCreate(BaseModel):
-    login: str
-    password: str
     first_name: str
     last_name: str
+    email: EmailStr
+    password: str
+
+    @validator('password')
+    @classmethod
+    def validate_password(cls, value):
+        password_length = len(value)
+        if password_length < 8 or password_length > 24:
+            raise ValueError('The password must be between 8 and 24 characters long')
+        return value
+
+    @validator('first_name')
+    @classmethod
+    def first_name_contains_only_letters(cls, value):
+        if not value.isalpha():
+            raise ValueError('The first name must contain only alphabethical symbols')
+        return value
+
+    @validator('last_name')
+    @classmethod
+    def last_name_contains_only_letters(cls, value):
+        if not value.isalpha():
+            raise ValueError('The last name must contain only alphabethical symbols')
+        return value
 
 
 class UserInDB(BaseModel):
